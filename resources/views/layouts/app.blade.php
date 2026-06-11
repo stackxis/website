62df2a1de59ae5f7b8b0411e6cdce6ba2a9e5877
Tharindu Wijayarathna <wikum.dev@gmail.com>
2026-06-11 14:25:45 +0530

    chore: remove unused favicon assets and controller, update .htaccess for static file serving

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,12 @@
     <meta name="theme-color" content="#1a3a6e">
     <link rel="canonical" href="{{ $pageUrl }}">
     <link rel="sitemap" type="application/xml" title="Sitemap" href="{{ route('sitemap') }}">
-    <link rel="icon" href="{{ asset('images/favicons/favicon.ico') }}" sizes="any">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon-16x16.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-touch-icon.png') }}">
-    <link rel="manifest" href="{{ route('manifest') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="48x48">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <meta property="og:site_name" content="Stackxis">
     <meta property="og:type" content="website">
     <meta property="og:locale" content="en_US">
@@ -38,7 +39,7 @@
             '@type' => 'Organization',
             'name' => 'Stackxis',
             'url' => route('home'),
-            'logo' => asset('images/logo.png'),
+            'logo' => asset('android-chrome-512x512.png'),
             'description' => 'Senior engineering studio building dependable software for ambitious teams.',
             'email' => 'hello@stackxis.com',
             'sameAs' => [],
